61ec68621f0bcf15ad7f16684a2aadc99ad5a2a4
Vinicius <vfernandespeixoto@gmail.com>
2014-11-01 13:25:53 -0200

    template

--- tubelike/urls.py
+++ tubelike/urls.py
@@ -3,10 +3,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tubelike.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns('movie',
+    url(r'^$', 'views.index'),
 )
