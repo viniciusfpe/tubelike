61f95dcb531a16edd11f268b1e3aa6a2e84c199a
Vinicius <vfernandespeixoto@gmail.com>
2014-11-05 11:13:44 -0200

    settings

--- tubelike/settings.py
+++ tubelike/settings.py
@@ -80,18 +80,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-#STATIC_URL = '/static/'
-
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR,'static'),
-#    )
-
-
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-PROJECT_DIR = os.path.join(PROJECT_ROOT,'../blogapp')
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT,'staticfiles/')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR,'static/'),
-)
+    os.path.join(BASE_DIR,'static'),
+    )
